2a82708a574f8dda9d91a94b174766ccdd2b1d2e
SynCROSS <51585533+SynCROSS@users.noreply.github.com>
2020-11-05 13:07:29 +0900

    define function

--- main.go
+++ main.go
@@ -5,8 +5,12 @@ import (
 	"learning-go/something"
 )
 
+func multiply(num1, num2 int) (int, string) { // * == num1 int , num2 int
+	return num1 * num2, "multiplied!"
+}
 func main() {
 	name := "SynCROSS" // * var name string = "Syncross"
 	something.SayHi()
 	fmt.Println(name) // * To export a function, the function name must begin with a capital letter.
+	fmt.Println(multiply(10, 10))
 }
