2d843544345e29745c0c3416c29d7f829af37ea6
SynCROSS <51585533+SynCROSS@users.noreply.github.com>
2020-11-05 13:12:46 +0900

    Ignore value

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ func multiply(num1, num2 int) (int, string) { // * == num1 int , num2 int
 	return num1 * num2, "-- multiplied!"
 }
 func main() {
-	result, message := multiply(10, 10) // * var name string = "Syncross"
+	result, _ := multiply(10, 10) // * var name string = "Syncross"
 	something.SayHi()
-	fmt.Println(result, message)
+	fmt.Println(result)
 }
