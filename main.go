d222f8348f358ca55722aba82cc9ceeebac4c42b
SynCROSS <51585533+SynCROSS@users.noreply.github.com>
2020-11-05 11:42:43 +0900

    define variable
    Learn how to define variable

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 )
 
 func main() {
-	fmt.Println("Hello world") // * To export a function, the function name must begin with a capital letter.
+	name := "SynCROSS" // * var name string = "Syncross"
 	something.SayHi()
+	fmt.Println(name) // * To export a function, the function name must begin with a capital letter.
 }
