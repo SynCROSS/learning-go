5779fbb9ef1386868e727442df3c9f2e27f8cffe
SynCROSS <51585533+yuto3125@users.noreply.github.com>
2020-11-09 08:41:39 +0900

    for loop

--- main.go
+++ main.go
@@ -2,16 +2,16 @@ package main
 
 import (
 	"fmt"
-	"strings"
 )
 
-func lenAndUPPER(word string) (length int, UPPERCASE string) {
-	defer fmt.Println("I'm done. :)")
-	fmt.Println("I'm started!")
-	length = len(word)
-	UPPERCASE = strings.ToUpper(word)
-	return length, UPPERCASE
+func addSomething(numbers ...int) int {
+  total:=0
+  for _, number:=range numbers{
+    total+=number
+  }
+  return total
 }
+
 func main() {
-	fmt.Println(lenAndUPPER("yee"))
+	fmt.Println(addSomething(1,2,3,4,5,6,7,8,9,10))
 }
