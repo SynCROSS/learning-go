a0822a69a99b402d2db7f456ed972341f31f824e
SynCROSS <51585533+yuto3125@users.noreply.github.com>
2020-11-11 10:43:03 +0900

    Array and Slice

--- main.go
+++ main.go
@@ -5,8 +5,10 @@ import (
 )
 
 func main() {
-	a := 10
-	b := &a // * Modify b to modify a
-	*b *= 10
-	fmt.Println(a)
+	array := [10]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10} // * This is the array in Gp
+	slice := []string{"s", "l", "i", "c", "e"}      // * This is the Slice
+	slice = append(slice, "🔪") // * Because append returns new array
+
+	fmt.Println(array)
+	fmt.Println(slice)
 }
