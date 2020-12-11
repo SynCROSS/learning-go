4a92d9c6de80cc5e67112a6cb7333550c997133b
SynCROSS <51585533+yuto3125@users.noreply.github.com>
2020-12-11 09:17:09 +0900

    Custom Error

--- main.go
+++ main.go
@@ -6,7 +6,9 @@ import (
 )
 
 func main() {
-  account1 := accounts.CreateAccount("SynCROSS")
-  fmt.Println(account1) // *  &{SynCROSS 0} -- returned Object's address
-  // * https://stackoverflow.com/questions/56112289/how-to-print-the-address-of-struct-variable-in-go
+	account1 := accounts.CreateAccount("SynCROSS")
+	account1.Deposit(1000000000)
+  fmt.Println(account1.GetBalance())
+  account1.Withdraw(1000000000 + 1)
+  fmt.Println(account1.GetBalance()) // * It Maybe Same Result because of an error.
 }
