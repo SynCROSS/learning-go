bbe419bce2fb14b2478ecdc5179e1e438f484bf8
SynCROSS <51585533+yuto3125@users.noreply.github.com>
2020-12-11 10:19:41 +0900

    Show an Error

--- main.go
+++ main.go
@@ -2,13 +2,18 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"main/accounts"
 )
 
 func main() {
 	account1 := accounts.CreateAccount("SynCROSS")
 	account1.Deposit(1000000000)
-  fmt.Println(account1.GetBalance())
-  account1.Withdraw(1000000000 + 1)
-  fmt.Println(account1.GetBalance()) // * It Maybe Same Result because of an error.
+	fmt.Println(account1.GetBalance())
+	e := account1.Withdraw(1000000000 + 1)
+	if e != nil {
+		// * Exit program after println
+		log.Fatalln(e) // * It Maybe print Error.
+	}
+	fmt.Println(account1.GetBalance()) // * It Maybe Same Result because of an error.
 }
