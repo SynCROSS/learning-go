abdfac348d9d31498bd71e2c90d3e67862f15856
SynCROSS <51585533+yuto3125@users.noreply.github.com>
2020-12-28 21:09:03 +0900

    Run Counter from Echo

--- server.go
+++ server.go
@@ -1,15 +1,13 @@
 package main
 
 import (
-	"net/http"
-
 	"github.com/labstack/echo/v4"
 )
 
 func main() {
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "echo 'Hello World'")
+		return c.File("index.html")
 	})
 	e.Logger.Fatal(e.Start(":1323")) // * 1323 is the port of the page
 }
